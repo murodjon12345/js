dd4dc8f8dd86b1151a37feeb8df12c5ec2fc3fc4
Murodjon <Javascript_murod@gmail.com>
2022-03-26 20:47:32 -1200

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let salom = prompt("Ismingizni kiriting:")
+console.log(`Mening Ismim: ${salom}`)
+
+let salom1 = prompt("Yoshingizni kiriting:")
+console.log(`Yoshim: ${salom1} da`)
+
+let salom3 = prompt("Misolni yeching 6 + 3 = ?:")
+console.log(`1. Misoldagi jovobingiz: 6 + 3 = ${salom3}`)
+
+let salom4 = prompt("Misolni yeching 15 - 5 = ?:")
+console.log(`2. Misoldagi jovobingiz: 15 + 5 = ${salom4}`)
+
+let salom5 = prompt("Misolni yeching 10 / 5 = ?:")
+console.log(`3. Misoldagi jovobingiz: 10 / 5 = ${salom5}`)
+
+let salom6 = prompt("Misolni yeching 10 % 4 = ?:")
+console.log(`4. Misoldagi jovobingiz: 10 % 4 = ${salom6}`)
+
+alert("jovobingizni bilish uchun 'console'ni oching")
